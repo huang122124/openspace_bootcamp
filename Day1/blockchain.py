70e132913b6a53d0e5b06212aa8f00fa76f40270
huang122124 <1394697259@qq.com>
2024-01-09 17:58:25 +0800

    day2 publish

--- Day1/blockchain.py
+++ Day1/blockchain.py
@@ -19,50 +19,61 @@ block = {
     'previous_hash': "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824"
 }
 '''
+class Block:
+    def __init__(self,index,transactions,timestamp,previous_hash):
+        self.index = index
+        self.transactions = transactions
+        self.timestamp = timestamp
+        self.previous_hash = previous_hash # Adding the previous hash field
+    def compute_hash(self):
+        block_string = json.dumps(self.__dict__ , sort_keys=True)
+        return hashlib.sha256(block_string.encode()).hexdigest()
+
 class Blockchain():
+    difficulty = 4 # difficulty of PoW algorithm
     def __init__(self):
-        self.current_transactions = []
         self.chain = []
-
-    def new_block(self,proof,previous_hash):
-        block = {
-            'index': len(self.chain)+1,
-            'timestamp': time(),
-            'transactions': self.current_transactions,
-            'proof': proof,
-            'previous_hash':previous_hash
-        }
-        self.current_transactions = []
-        self.chain.append(block)
-        return block
-
-    def new_transaction(self,sender,recipient,amount):
-        transaction = {
-            'sender': sender,
-            'recipient': recipient,
-            'amount': amount,
-        }
-        self.current_transactions.append(transaction)
+        self.create_genesis_block()
+    def create_genesis_block(self):
+        new_block = Block(0,[],time(),"0")
+        new_block.hash = new_block.compute_hash()
+        self.chain.append(new_block)
 
 
-    def hash(block):
-        data = json.dumps(block).encode()
-        return hashlib.sha3_256(data).hexdigest()
+    def add_block(self,block,proof):
+        if self.last_block.hash !=block.previous_hash:
+            return False
+        if not Blockchain.is_valid_proof(block,proof):
+            return False
 
+
+    def add_new_transaction(self, transaction):
+        self.unconfirmed_transactions.append(transaction)
+
+    def is_valid_proof(self,block,block_hash):
+        """
+                Check if block_hash is valid hash of block and satisfies
+                the difficulty criteria.
+                """
+        return (block.compute_hash() == block_hash) and block_hash[:4]==('0' * Blockchain.difficulty)
+    
     @property
     def last_block(self):
         return self.chain[-1]
-
-
-
-    #找到一个数字 P ，使得它与前一个区块的 Proof 拼接成的字符串的 Hash 值以 4 个零开头。
+    
     @staticmethod
-    def proof_of_work(self,proof):
-        p = 0
-        while hashlib.sha256(f'{proof}{p}'.encode()).hexdigest()[:4] != '0000':
-            p += 1
-        return p
-
+    def proof_of_work(self,block):
+        """
+            简单的工作量证明:
+                 - 查找一个 p' 使得 hash(pp') 以4个0开头
+                 - p 是上一个块的证明,  p' 是当前的证明
+                """
+        block.nonce = 0
+        hash = block.compute_hash()
+        while hash[:4]!=('0' * Blockchain.difficulty):
+            block.nonce +=1
+            hash = block.compute_hash()
+        return hash
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
