034318d28d85d288e545e564bb2c8593447c36f3
huang122124 <1394697259@qq.com>
2024-01-28 16:48:47 +0800

    finish NFT whitelist offline signature

--- ERC2612/README.md
+++ ERC2612/README.md
@@ -1,13 +1,9 @@
-# Sample Hardhat Project
-
-This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, and a script that deploys that contract.
-
-Try running some of the following tasks:
-
-```shell
-npx hardhat help
-npx hardhat test
-REPORT_GAS=true npx hardhat test
-npx hardhat node
-npx hardhat run scripts/deploy.js
-```
+对多个地址用户进行签名，在合约中进行验签
+signer:0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266
+addr1:0x70997970C51812dc3A010C7d01b50e0d17dc79C8
+nft_market deployed to :0x5FbDB2315678afecb367f032d93F642f64180aa3
+msg_hash:0x3bfd32663860f9ee3772b2ee0d6191e26ffb83837c1c8dd3a55dda35d8fc2a52
+期望验证成功： true
+    ✔ verify sign (788ms)
+    
+  1 passing (793ms)
---
+++ ERC2612/test/signTest.js
@@ -0,0 +1,31 @@
+const {
+  time,
+  loadFixture,
+} = require("@nomicfoundation/hardhat-toolbox/network-helpers");
+const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("对多个地址用户进行签名，在合约中进行验签", function () {
+  it("verify sign", async function () {
+    // Contracts are deployed using the first signer/account by default
+    const [signer, addr1] = await ethers.getSigners()
+    const white_list = [addr1.address, '0x123456', '0x654321']    //WhitleList
+    const nft_market = await ethers.deployContract("Permit_NftMarketplace", ['0xf0f93144CECa5F5bbE6B953bDC3dD4991c2Ab7d3', '0x9bE7bbB4659109E56EdC7637A5619DCB5B9a43bF'])
+    console.log("signer:"+signer.address);
+    console.log("addr1:"+addr1.address);
+    const nft = await nft_market.getAddress()
+    console.log("nft_market deployed to :"+nft);
+    expect(white_list).to.include(addr1.address)    //判断是否在白名单内。实际应发送用户地址到后端进行判断
+    // 对消息进行签名
+    const salt = 888
+    const msg_hash = ethers.solidityPackedKeccak256(["address","uint256"], [ addr1.address,salt]);
+    const sign = await signer.signMessage(ethers.getBytes(msg_hash));
+    console.log("msg_hash:"+msg_hash);
+    //调用合约进行验证
+    let verified = await nft_market.verify_signature(addr1.address,salt,sign)
+    console.log("期望验证成功：", verified);
+    expect(verified).to.equal(true);
+
+  })
+});
