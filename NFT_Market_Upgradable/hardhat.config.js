6d11200f7fba1d8f1da982a429f5e84d5d889e1e
huang122124 <1394697259@qq.com>
2024-02-16 18:15:24 +0800

    change little

--- NFT_Market_Upgradable/hardhat.config.js
+++ NFT_Market_Upgradable/hardhat.config.js
@@ -6,12 +6,12 @@ module.exports = {
   solidity: "0.8.23",
   networks: {
     sepolia: {
-      url: `https://sepolia.infura.io/v3/3b8a4ff7ba6240e7b42be7f38d44e346`,
-      accounts: ['8f5f5c4a6abf7ff59f4d48bc35c3b8ec0dd69d333354f8e397d6f2babca7d0b2']
+      url: `https://sepolia.infura.io/v3/xxxx`,
+      accounts: ['']
     }
   },
   etherscan: {
-    apiKey: "M2J52SCD1GJ4633TZ495MDRPIMPVYZZWF6",
+    apiKey: "",
   },
   sourcify: {
     enabled: true
